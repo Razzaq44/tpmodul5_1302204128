870a78176510759466ba81312660ace531334543
razzaq <razzaq@student.telkomuniversity.ac.id>
2022-03-25 15:48:15 +0700

    implementasi design by contract

--- tpmodul5_1302204128/Program.cs
+++ tpmodul5_1302204128/Program.cs
@@ -1,9 +1,16 @@
-﻿public class program
+﻿using System.Diagnostics;
+using System.Diagnostics.Contracts;
+
+public class program
 {
     public static void Main(string[] args)
     {
         SayaTubeVideo vid = new SayaTubeVideo("Tutorial Design By Contract - [Razzaq Adi Wibowo]");
-        vid.PrintVideoDetails();
+        for (int i = 0; i < 220; i++)
+        {
+            vid.IncreasePlayCount(10000000);
+            vid.PrintVideoDetails();
+        }
     }
 }
 
@@ -15,6 +22,8 @@ public class SayaTubeVideo
 
     public SayaTubeVideo(string title)
     {
+        Contract.Requires(title != null);
+        Debug.Assert(title.Length > 0 && title.Length <= 100, "Judul video maks 100 karakter dan tidak boleh kosong");
         this.title = title;
         Random r = new Random();
         this.id = r.Next(99999);
@@ -23,14 +32,30 @@ public class SayaTubeVideo
 
     public void IncreasePlayCount(int playCount)
     {
-        this.playCount += playCount;
+        Contract.Requires(playCount != null);
+        Debug.Assert(playCount <= 10000000, "Maks playcount 10000000");
+        int cek;
+        try
+        {
+            cek = checked(this.playCount + playCount);
+            this.playCount += playCount;
+        }
+        catch (OverflowException error)
+        {
+            Console.WriteLine();
+            Console.WriteLine("---------------================ Integer Overflow ================---------------");
+            this.playCount = int.MaxValue;
+        }
     }
 
     public void PrintVideoDetails()
     {
+        Console.WriteLine();
         Console.WriteLine("id: " + this.id);
         Console.WriteLine("title: " + this.title);
         Console.WriteLine("playCount: " + this.playCount);
+        Console.WriteLine();
+        Console.WriteLine("------------------------------------------------------------------------------------");
     }
 
 
