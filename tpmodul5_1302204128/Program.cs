82d01ece7c07c85fd6cd6da5fd147f9310f010a7
razzaq <razzaq@student.telkomuniversity.ac.id>
2022-03-25 14:25:55 +0700

    menambahkan class SayaTubeVideo

--- tpmodul5_1302204128/Program.cs
+++ tpmodul5_1302204128/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class program
+{
+    public static void Main(string[] args)
+    {
+        SayaTubeVideo vid = new SayaTubeVideo("Tutorial Design By Contract - [Razzaq Adi Wibowo]");
+        vid.PrintVideoDetails();
+    }
+}
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        this.title = title;
+        Random r = new Random();
+        this.id = r.Next(99999);
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int playCount)
+    {
+        this.playCount += playCount;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("id: " + this.id);
+        Console.WriteLine("title: " + this.title);
+        Console.WriteLine("playCount: " + this.playCount);
+    }
+
+
+}
